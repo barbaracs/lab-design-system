ee1172357b5cc9f1afb934002d12521d238ad6c7
barbara carvalho <barbaracarvalho@barbaras-MacBook-Pro.local>
2022-10-13 21:59:54 -0300

    feat: create checkbox component using radix

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Checkbox, CheckboxProps } from './Checkbox'
+import { Text } from '../Text/Text'
+
+export default {
+    title: 'Components/Checkbox',
+    component: Checkbox,
+    args: {},
+    argTypes: {
+        asChild: {
+            table: {
+                disable: true,
+            }
+        },
+    },
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {}
+
+export const CheckboxWithText: StoryObj<CheckboxProps> = {
+    decorators: [
+        (Story) => {
+            return (
+                <div className='flex items-center gap-2'>
+                    {Story()}
+                    <Text size="sm">
+                        Lembrar de mim por 30 dias
+                    </Text>
+                </div>
+            )
+        }
+    ]
+}
